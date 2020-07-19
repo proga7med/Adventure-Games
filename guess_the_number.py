705dc12480da90a4fb3a0104ea5ec9d27a650927
pro,Og <progahmedelsaeed@hotmail.com>
2020-07-19 22:21:06 +0200

    Add Guess the number game

---
+++ guess_the_number.py
@@ -0,0 +1,15 @@
+import random
+
+number = random.randrange(1, 50)
+guess = int(input("Guess a number between 1 to 50: "))
+
+while guess != number:
+    if guess < number:
+        print("You need to guess higher. Try again.\n")
+        guess = int(input("Guess a number between 1 to 50: "))
+    else:
+        print("You need to guess lower. Try again.\n")
+        guess = int(input("Guess a number between 1 to 50: "))
+
+print("You guessed the number correctly!")
+
